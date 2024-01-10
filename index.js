cfbe8ecef7c5eda3b4804d7667e0bc832b9dce3b
Imagination Rabbit <mdotstrange@gmail.com>
2024-01-10 11:22:15 -0800

    testing god speak

--- index.js
+++ index.js
@@ -9,20 +9,39 @@ wss.on('connection', function connection(ws)
   ws.on('message', function message(data) 
   {
     console.log('received: %s', data);
-    ReceivedMessage(data);
+    ReceivedMessage(data, ws);
   });
 
-  ws.send('something');
+  ws.send('ConnectedToSocketServer');
 
+/*
   setInterval(async () => 
   {
     const cpuTemp = JSON.stringify((await si.currentLoad()).avgLoad);
     ws.send(cpuTemp);
   }, 9000);
+  */
+
 });
 
-function ReceivedMessage( msg)
+function ReceivedMessage(msg,ws)
 {
+
+  let firstThreeChars = msg.substring(0, 3);
+
+  if(firstThreeChars == "God")
+  {
+    let start = str.indexOf('*') + 1; // position after '*'
+    let end = str.indexOf('_'); // position of '_'
+    let extractedString = str.substring(start, end);
+
+    if(extractedString == "Speaks")
+    {
+      ws.send(msg);
+    }
+
+  }
+
 if(msg == "hello")
 {
 console.log("HELLO RECEIVED")
